ba7de5a1e4e29e423326c15085bfe19489bf49d8
Bart Massey <bart.massey@gmail.com>
2020-04-09 02:07:54 -0700

    added db/linear button

--- soundfreq.py
+++ soundfreq.py
@@ -2,11 +2,7 @@ from tkinter import *
 from scipy import fftpack
 import math, numpy, struct, sys, wave
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-window = Tk()
-window.title("soundfreq")
-window.protocol("WM_DELETE_WINDOW", window.quit)
+from matplotlib.backends.backend_tkagg import *
 
 def read_wave(filename):
     w = wave.open(filename, 'rb')
@@ -22,6 +18,10 @@ def read_wave(filename):
     w.close()
     return (info, float_samples)
 
+window = Tk()
+window.title("soundfreq")
+window.protocol("WM_DELETE_WINDOW", window.quit)
+
 info, samples = read_wave(sys.argv[1])
 
 size = 4096
@@ -32,16 +32,51 @@ for i in range(size):
 myfft = fftpack.fft(samples)
 assert len(myfft) == size
 
+def linear_scale(y):
+    return y
+
+def db_scale(y):
+    return 20 * math.log10(y)
+
+
 # https://matplotlib.org/3.2.1/gallery/user_interfaces/
 #   embedding_in_tk_sgskip.html
 fig = plt.figure(figsize=(12, 8), dpi=100)
 fplot = fig.add_subplot(1, 1, 1)
 xs = [info.framerate * x / size for x in range(size // 2)]
-ys = [20 * math.log10(abs(myfft[i] * 2 / size)) for i in range(size // 2)]
-fplot.plot(xs, ys)
+scale = db_scale
+def plot_ys():
+    ys = [scale(abs(myfft[i] * 2 / size)) for i in range(size // 2)]    
+    fplot.clear()
+    fplot.plot(xs, ys)
+
+plot_ys()
+
+ampl_mode = "dB"
+def change_mode():
+    global ampl_mode, button, scale, canvas
+    if ampl_mode == "dB":
+        ampl_mode = "linear"
+        scale = linear_scale
+    elif ampl_mode == "linear":
+        ampl_mode = "dB"
+        scale = db_scale
+    else:
+        assert False
+    plot_ys()
+    canvas.draw()
+    button.configure(text=ampl_mode)
 
 canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
+toolbar = NavigationToolbar2Tk(canvas, window)
+toolbar.update()
+button = Button(
+    master=window,
+    text=ampl_mode,
+    command=change_mode,
+)
+button.pack(side=BOTTOM)
 canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
 window.mainloop()
